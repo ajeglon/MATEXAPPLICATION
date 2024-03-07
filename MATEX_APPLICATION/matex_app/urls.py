7da183e16e8da318b8a5b18b9ddf9b3ea2d2d55c
ajeglon <61421392+ajeglon@users.noreply.github.com>
2024-03-07 04:09:36 +0000

    initial html templates and url setup

--- MATEX_APPLICATION/matex_app/urls.py
+++ MATEX_APPLICATION/matex_app/urls.py
@@ -1,7 +1,13 @@
+from django.contrib import admin
 from django.urls import path
+from .views import Index, SignUpView, Dashboard
+from django.contrib.auth import views as auth_views
 
-from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', Index.as_view(), name='index'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(template_name='matex_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='matex_app/logout.html'), name='logout'),
 ]
--- MATEX_APPLICATION/matex_app/views.py
+++ MATEX_APPLICATION/matex_app/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login
+
+from .forms import UserRegistrationForm
+
+
+class Index(TemplateView):
+    template_name = 'matex_app/index.html'
+
+
+class Dashboard(View):
+    def get(self, request):
+        return render(request, 'matex_app/dashboard.html')
+
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'matex_app/signup.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+
+        return render(request, 'matex_app/signup.html', {'form': form})
